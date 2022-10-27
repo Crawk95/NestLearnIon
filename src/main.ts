7dc9a5ac2a0866bff31b00b6245796edca58f6e3
Ion M <i.mustafenco@gmail.com>
2022-10-27 11:59:54 +0300

    #5-Setup security

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet())
+  app.enableCors({
+    origin: JSON.parse(process.env.ALLOWED_ORIGIN),
+    optionsSuccessStatus: 200,
+  });
 
   const config = new DocumentBuilder()
       .setTitle('NestJs Project basic')
